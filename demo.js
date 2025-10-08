ac7e60566e14b79fcb9b9570f94b7d99eec8eccc
thinhpham23 <phamngocthinhqng23@gmail.com>
2025-10-08 10:21:18 +0700

    update ngày 10/08/2025

--- demo.js
+++ demo.js
@@ -1,3 +1,26 @@
+console.log("hello world!")
+
 let name = "thinh"
 console.log(name)
-console.log("hello world!")
+
+// nhập ,xuất dữ liệu 
+// cách 1 : Dùng Readline(module) có sẵn
+// khai báo thư viện 
+const readline = require('readline');
+// hoặc import readline from 'node:readline';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output:process.stdout
+
+});
+
+rl.question('Nhập tên của bạn :',(ten)=>{ // hàm mũi tên với ten là tham số
+        console.log('Xin chào '+ ten);  // hoặc console.log(`Xin chào ${ten}`); với ` là dấu backtick
+        rl.close();                       // tương đương console.log('Xin chào ', ten);
+})
+// cách 2 dùng thư viện bên ngoài (phải cài )(prompt-sync) gọn hơn 
+
+const prompt = require('prompt-sync')();
+let ten = prompt("nhập tên :");
+console.log("chào "+ ten);
+
